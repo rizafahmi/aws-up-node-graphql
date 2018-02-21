899e5efbe45a4ee849960569c769ddf0fc060acf
Riza Fahmi <rizafahmi@gmail.com>
2018-02-22 06:14:33 +0700

    Finish graphql

--- schema.js
+++ schema.js
@@ -3,9 +3,54 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = require("graphql");
 
+const HomeworldType = new GraphQLObjectType({
+  name: "homeworld",
+  description: "Planet instance",
+  fields: () => ({
+    name: {
+      type: GraphQLString,
+      resolve: planet => planet.data.name
+    },
+    climate: {
+      type: GraphQLString,
+      resolve: planet => planet.data.climate
+    },
+    gravity: {
+      type: GraphQLString,
+      resolve: planet => planet.data.gravity
+    },
+    population: {
+      type: GraphQLString,
+      resolve: planet => planet.data.population
+    }
+  })
+});
+const CharacterType = new GraphQLObjectType({
+  name: "characters",
+  description: "...",
+  fields: () => ({
+    name: {
+      type: GraphQLString,
+      resolve: character => character.data.name
+    },
+    birthYear: {
+      type: GraphQLString,
+      resolve: character => character.data.birth_year
+    },
+    gender: {
+      type: GraphQLString,
+      resolve: character => character.data.gender
+    },
+    homeworld: {
+      type: HomeworldType,
+      resolve: character => axios(character.data.homeworld)
+    }
+  })
+});
 const FilmType = new GraphQLObjectType({
   name: "Film",
   description: "...",
@@ -13,6 +58,11 @@ const FilmType = new GraphQLObjectType({
     title: {
       type: GraphQLString,
       resolve: film => film.data.title
+    },
+    characters: {
+      type: new GraphQLList(CharacterType),
+      resolve: character =>
+        Promise.all(character.data.characters.map(char => axios(char)))
     }
   })
 });
