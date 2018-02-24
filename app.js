f6db1275910092058a656096beffb1e2e29a4489
Riza Fahmi <rizafahmi@gmail.com>
2018-02-24 21:50:32 +0700

    Support for CORS

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const app = require("express")();
 const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
 
 const schema = require("./schema");
 
 const { PORT = 3000 } = process.env;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ status: "OK", source: "development" });
